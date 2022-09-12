36e7e5f8e4de5f822aee2ca2d345db9e7a6e01d1
Macarena Ruiz <contactomruiz@yahoo.com>
2022-09-12 10:52:18 -0300

    ej 3

--- Solución-Clase-03/Assets/Scripts/Ejercicio03.cs
+++ Solución-Clase-03/Assets/Scripts/Ejercicio03.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Ejercicio03 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public float x;
+    public float Z;
+    bool esta_Saltando;
+    private Rigidbody2D rb2d;
+
+        private void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        Movimiento();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Movimiento()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            float movimiento_x = Input.GetAxis("Horizontal");
+            rb2d.velocity = new Vector2(x, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            float movimiento_x = Input.GetAxis("Horizontal");
+            rb2d.velocity = new Vector2(x, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Z += Time.deltaTime * 10;
+            transform.rotation = Quaternion.Euler(0, 0, Z);
+        }
+
+        if (Input.GetButton("Jump") && !esta_Saltando)
+        {
+            rb2d.AddForce(Vector2.up * speed);
+            Debug.Log("Estoy saltando Wiiii");
+
+            esta_Saltando = true;
+        }
     }
 }
